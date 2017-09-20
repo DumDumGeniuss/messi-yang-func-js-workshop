c26a06bc4eb0c4882b02f2fcb9afe4491ec0cbd9
DumDumGeniuss <dumdumgenius@gmail.com>
2017-09-20 16:59:34 +1000

    Add solution 16

--- sols/recursion.js
+++ sols/recursion.js
@@ -1,8 +1,25 @@
 function getDependencies(tree) {
-  // SOLUTION GOES HERE
-  // Note: Feel free to add additional arguments
-  // to this function for use with recursive calls.
-  // Or not! There are many ways to recurse.
+  if (!tree) {
+    return [];
+  }
+  var depends = [];
+  Object.keys(tree.dependencies || []).forEach(function (key) {
+    var dependObj = tree.dependencies[key];
+    depends.push(key + '@' + dependObj.version);
+
+    // Get all sub dependencies of dependObj
+    var childDepends = getDependencies(dependObj);
+
+    // There may be duplicated dependency
+    childDepends.forEach(function (childDepend) {
+      if (depends.indexOf(childDepend) === -1) {
+        depends.push(childDepend);
+      }
+    });
+  });
+
+  // Sort array
+  return depends.sort();
 }
 
-module.exports = getDependencies
+module.exports = getDependencies;
